80245ecb392ac08fc20505c3767a0e6e96271582
Guilherme Cazaroto <gui.cazaroto@gmail.com>
2020-11-09 22:59:55 -0300

    tsc

---
+++ index.js
@@ -0,0 +1,9 @@
+var n1 = document.querySelector('#n1');
+var n2 = document.querySelector('#n2');
+var btn = document.querySelector('#btn-sum');
+function sum(n1, n2) {
+    return n1 + n2;
+}
+btn.addEventListener('click', function () {
+    console.log(sum(Number(n1.value), Number(n2.value)));
+});
---
+++ index.ts
@@ -0,0 +1,13 @@
+const n1 = document.querySelector('#n1') as HTMLInputElement
+const n2 = document.querySelector('#n2') as HTMLInputElement
+const btn = document.querySelector('#btn-sum')
+
+function sum (n1: number, n2: number) {
+  return n1 + n2
+}
+
+btn.addEventListener('click', () => {
+  console.log(
+    sum(Number(n1.value), Number(n2.value))
+  )
+})
